564201a3b80f4c3297af1a24e12e01e3dc93398a
argumpamungkas <argumpamungkaass@gmail.com>
2025-05-26 11:25:06 +0700

    add verify token for auth

--- minigram-api/helpers/jwt.go
+++ minigram-api/helpers/jwt.go
@@ -1,21 +1,25 @@
 package helpers
 
 import (
+	"errors"
 	"log"
+	"strings"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
 
 var key = "key123Secret"
 
-func GenerateJWT(username string, email string) (res string, err error) {
+func GenerateJWT(id uint, username string, email string) (res string, err error) {
 
 	currentTime := time.Now().Unix()
 
 	claims := jwt.MapClaims{
-		"email":    email,
+		"id":       id,
 		"username": username,
+		"email":    email,
 		"time":     currentTime,
 	}
 
@@ -27,5 +31,38 @@ func GenerateJWT(username string, email string) (res string, err error) {
 	}
 	res = signedToken
 
+	return
+}
+
+func VerifyToken(ctx *gin.Context) (res interface{}, err error) {
+	header := ctx.Request.Header.Get("Authorization")
+	bearer := strings.HasPrefix(header, "Bearer")
+
+	if !bearer {
+		err = errors.New("invalid header")
+		return
+	}
+
+	keyFunc := func(j *jwt.Token) (res interface{}, err error) {
+		if _, ok := j.Method.(*jwt.SigningMethodHMAC); !ok {
+			err = errors.New("invalid method header")
+			return
+		}
+
+		res, err = []byte(key), nil
+		return
+	}
+
+	log.Println("str token ", header)
+
+	strToken := strings.Split(header, " ")[1]
+	token, err := jwt.Parse(strToken, keyFunc)
+
+	if _, ok := token.Claims.(jwt.MapClaims); !ok && !token.Valid {
+		err = errors.New("invalid token")
+		return
+	}
+
+	res = token.Claims.(jwt.MapClaims)
 	return
 }
